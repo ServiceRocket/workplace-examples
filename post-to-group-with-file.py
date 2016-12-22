62ae095be617bcce80f40028eb1d14e853fb3bd1
yclian <foss@yclian.com>
2016-12-22 16:40:34 +0800

    Clean up scripts to use environment variables and arguments. Indentation has also been changed from 4 spaces to 2 spaces.

--- post-to-group-with-file.py
+++ post-to-group-with-file.py
@@ -1,38 +1,40 @@
-import requests
 import json
+import os
+import requests
+import sys
 
-# Constants
+"""
+Simple script to post content and attachment to a Workplace group, similar to `post-to-group.py`.
+
+    $ ACCESS_TOKEN=... COMMUNITY_ID=... python post-to-group.py <user_id> <group_id> <file_path> "Hello world!"
+
+"""
+
 GRAPH_URL_PREFIX = 'https://graph.facebook.com/'
 
-access_token = ''
-community_id = ''
-impersonate_user_id = ''  # post as this user
-group_to_post_to_id = ''  # post to this group
-message_to_post = 'hello world'  # post this message
-file_to_post = 'logo.png'
-
-
-# Methods#
+access_token = os.environ['ACCESS_TOKEN']
+community_id = os.environ['COMMUNITY_ID']
+impersonate_user_id = sys.argv[1]
+group_to_post_to_id = sys.argv[2]
+file_to_post = sys.argv[3]
+message_to_post = sys.argv[4]
 
 def buildHeader(access_token):
-    return {'Authorization': 'Bearer ' + access_token}
-
+  return {'Authorization': 'Bearer ' + access_token}
 
 def postToGroupWithFile(access_token):
-    headers = buildHeader(access_token)
-    result = requests.get(GRAPH_URL_PREFIX + impersonate_user_id + '?fields=impersonate_token', headers=headers)
-    data = json.loads(result.text, result.encoding)
-    impersonate_token = data["impersonate_token"]
-    headers = buildHeader(impersonate_token)
+  headers = buildHeader(access_token)
+  result = requests.get(GRAPH_URL_PREFIX + impersonate_user_id + '?fields=impersonate_token', headers=headers)
+  data = json.loads(result.text, result.encoding)
+  impersonate_token = data["impersonate_token"]
+  headers = buildHeader(impersonate_token)
 
-    files = {'file': open(file_to_post, 'rb')}
-    data = {
-        "name": message_to_post
-    }
-    result = requests.post(GRAPH_URL_PREFIX + group_to_post_to_id + '/photos', headers=headers, files=files, data=data)
-
-    return json.loads(result.text, result.encoding)
-
+  files = {'file': open(file_to_post, 'rb')}
+  data = {
+    "name": message_to_post
+  }
+  result = requests.post(GRAPH_URL_PREFIX + group_to_post_to_id + '/photos', headers=headers, files=files, data=data)
+  return json.loads(result.text, result.encoding)
 
 result = postToGroupWithFile(access_token)
 print(result)
--- post-to-group.py
+++ post-to-group.py
@@ -1,37 +1,40 @@
-import requests
 import json
+import os
+import requests
+import sys
 
-# Constants
+"""
+Simple script to post content to a Workplace group by impersonating a person.
+
+    $ ACCESS_TOKEN=... COMMUNITY_ID=... python post-to-group.py <user_id> <group_id> "Hello world!"
+
+"""
+
 GRAPH_URL_PREFIX = 'https://graph.facebook.com/'
 
-access_token = ''
-community_id = ''
-impersonate_user_id = ''  # post as this user
-group_to_post_to_id = ''  # post to this group
-message_to_post = 'hello world'  # post this message
-
-
-# Methods#
+access_token = os.environ['ACCESS_TOKEN']
+community_id = os.environ['COMMUNITY_ID']
+impersonate_user_id = sys.argv[1]
+group_to_post_to_id = sys.argv[2]
+message_to_post = sys.argv[3]
 
 def buildHeader(access_token):
-    return {'Authorization': 'Bearer ' + access_token}
-
+  return {'Authorization': 'Bearer ' + access_token}
 
 def postToGroup(access_token):
-    headers = buildHeader(access_token)
-    result = requests.get(GRAPH_URL_PREFIX + impersonate_user_id + '?fields=impersonate_token', headers=headers)
-    data = json.loads(result.text, result.encoding)
-    impersonate_token = data["impersonate_token"]
-    headers = buildHeader(impersonate_token)
+  headers = buildHeader(access_token)
+  result = requests.get(GRAPH_URL_PREFIX + impersonate_user_id + '?fields=impersonate_token', headers=headers)
+  data = json.loads(result.text, result.encoding)
+  impersonate_token = data["impersonate_token"]
+  headers = buildHeader(impersonate_token)
 
-    data = {
-        "message": message_to_post,
-        "type": "status"
-    }
+  data = {
+    "message": message_to_post,
+    "type": "status"
+  }
 
-    result = requests.post(GRAPH_URL_PREFIX + group_to_post_to_id + '/feed', headers=headers, data=data)
-    return json.loads(result.text, result.encoding)
-
+  result = requests.post(GRAPH_URL_PREFIX + group_to_post_to_id + '/feed', headers=headers, data=data)
+  return json.loads(result.text, result.encoding)
 
 result = postToGroup(access_token)
 print(result)
